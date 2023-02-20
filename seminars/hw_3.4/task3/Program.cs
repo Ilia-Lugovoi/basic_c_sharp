55ecc7dbf025d3f5cc9f46459c46d477f567f127
Ilya <ilya.lugovoy012@gmail.com>
2023-02-20 12:20:35 +0300

    Add task 3 for 3 hw

---
+++ seminars/hw_3.4/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите значение: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int newNum = num + 1;
+int[] numbs = new int[newNum];
+
+Console.Write($"{num} -> ");
+for (int i = 1; i < numbs.Length; i++)
+{
+    numbs[i] = i;
+    Console.Write($"{Math.Pow(numbs[i], 3)} ");
+}
+
+Console.WriteLine();
