63927ce798cbdf991db5d59cba92077d55259ff9
Ilya <ilya.lugovoy012@gmail.com>
2023-02-20 12:16:57 +0300

    Add task1 for 3 hw

---
+++ seminars/hw_3.4/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.Write("Введите 5-значное число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int n1 = numb / 10000;
+
+if(n1 > 10 || n1 < 1)
+{
+    while(true)
+    {
+        Console.Write("Введите 5-значное число: ");
+        numb = Convert.ToInt32(Console.ReadLine());
+        n1 = numb / 10000;
+
+        if(n1 > 10 || n1 < 1)
+            continue;
+        else
+            break;
+    }
+}
+
+n1 = numb / 10000;
+int n2 = (numb % 10000) / 1000;
+int n4 = (numb % 100) / 10;
+int n5 = numb % 10;
+
+if(n1 == n5 && n2 == n4)
+    Console.WriteLine("Палиндром!");
+else
+    Console.WriteLine("Не палиндром");
