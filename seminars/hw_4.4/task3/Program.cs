4a80f044773e8a3ad6fd289281c0f7926333ff4d
Ilya <ilya.lugovoy012@gmail.com>
2023-02-22 20:01:59 +0300

    add task 3 for 4 hw

---
+++ seminars/hw_4.4/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] ar = new int[8];
+
+for (int i = 0; i < ar.Length; i++)
+{
+    ar[i] = new Random().Next(0, 101);
+    Console.Write($"{ar[i]} ");
+}
+
+Console.WriteLine(" ");
