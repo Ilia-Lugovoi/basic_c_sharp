49db4a4df625b34b76c54f19e874943f333bfc55
Ilya <ilya.lugovoy012@gmail.com>
2023-02-22 20:00:48 +0300

    add task 2 for 4 hw

---
+++ seminars/hw_4.4/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.Write("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while(numb != 0)
+{
+    sum = sum + numb % 10;
+    numb = numb / 10;
+}
+
+Console.WriteLine(sum);
