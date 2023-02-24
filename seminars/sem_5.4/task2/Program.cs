88cd9e979fb0fb1ebf90823319e86de5643c24d1
Ilya <ilya.lugovoy012@gmail.com>
2023-02-24 15:23:20 +0300

    Add task 2 for sem 5

---
+++ seminars/sem_5.4/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+
+int[] array = {6, 7, 19, 345, -3};
+int trueNumb = -3;
+
+void NumbMembershipСheck(int[] array, int trueNumb)
+{
+    int i = 0;
+    foreach (var numb in array)
+    {
+        if(numb == trueNumb)
+        {
+            i++;
+        }
+    }
+
+    if(i > 0)
+        Console.WriteLine("yes");
+    else if(i == 0)
+        Console.WriteLine("no");
+}
+
+NumbMembershipСheck(array, trueNumb);
