1a2e743d319800641d871b7503960490f24a25d7
Ilya <ilya.lugovoy012@gmail.com>
2023-02-25 13:25:55 +0300

    Add task 4 for sem 5

--- seminars/sem_5.4/task4/Program.cs
+++ seminars/sem_5.4/task4/Program.cs
@@ -4,4 +4,53 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
+Console.Write("Enter how many elements of the array you want to create: ");
+int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+
+int[] limits = {1, 10};
+int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(limits[0], limits[1]);
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+    return array;
+}
+
+int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
+
+if(lengthOfArray % 2 == 0)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i] * array[lengthOfArray - 1]);
+        lengthOfArray--;
+        if(lengthOfArray - 1 == i)
+        {
+            break;
+        }
+    }
+}
+else
+{
+    int tempLength = (lengthOfArray / 2);
+    lengthOfArray = lengthOfArray - 1;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i] * array[lengthOfArray]);
+        lengthOfArray--;
+        if(lengthOfArray - 1 == i)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine(array[tempLength]);
+
+}
 
