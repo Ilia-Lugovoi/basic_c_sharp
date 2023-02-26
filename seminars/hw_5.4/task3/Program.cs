8265a7736be9e46fe2302fdf5ed269fce9adab7c
Ilya <ilya.lugovoy012@gmail.com>
2023-02-26 13:15:14 +0300

    Add task 3 for hw 5

---
+++ seminars/hw_5.4/task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.Write("Enter how many elements of the array you want to create: ");
+int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+
+int[] limits = {-100, 101};
+int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(limits[0], limits[1]);
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+    return array;
+}
+
+int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
+int max = array[0];
+int min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    else if(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine(max - min);
