5c654ad4ff1c6908c5689ac05c46ee3a4601c9d8
Ilya <ilya.lugovoy012@gmail.com>
2023-02-26 11:58:57 +0300

    Add task 1 for hw 5

---
+++ seminars/hw_5.4/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Enter how many elements of the array you want to create: ");
+int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+int[] limits = {100, 1000};
+int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(limits[0], limits[1]);
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+    return array;
+}
+
+int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 != 0)
+        count++;
+}
+
+Console.WriteLine(count);
