5cd4eeab738b3a4a37b6ef858f46aa7d7ed24628
Ilya <ilya.lugovoy012@gmail.com>
2023-02-28 10:37:38 +0300

    Add task 1 for sem 6

--- lectures/Example013_Recursion/Program.cs
+++ lectures/Example013_Recursion/Program.cs
@@ -1,13 +1,25 @@
 ﻿string[,] table = new string[2,5];
 
 
-table[1, 2] = "World";
-table[0, 2] = "hello";
+// table[1, 2] = "World";
+// table[0, 2] = "hello";
 
-for (int rows = 0; rows < 2; rows++)
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"-{table[rows, colums]}-");
+//     }
+// }
+
+int[,] matrix = new int[3,4];
+
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
 {
-    for (int colums = 0; colums < 5; colums++)
+    for (int colums = 0; colums < matrix.GetLength(1); colums++)
     {
-        Console.WriteLine($"-{table[rows, colums]}-");
+        Console.Write($"{matrix[rows, colums]} ");
     }
+
+    Console.WriteLine();
 }
