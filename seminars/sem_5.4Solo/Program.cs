fa4919793157b06497eb59bf4841faf336f8295e
Ilya <ilya.lugovoy012@gmail.com>
2023-03-01 11:53:48 +0300

    Add solo work of sem 5.4

---
+++ seminars/sem_5.4Solo/Program.cs
@@ -0,0 +1,127 @@
+﻿// // Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// // Найдите сумму отрицательных и положительных элементов массива.
+// // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20
+
+// int[] randNums = new int[12];
+// int sumPos = 0;
+// int sumNeg = 0;
+
+// for (int i = 0; i < randNums.Length; i++)
+// {
+//     randNums[i] = new Random().Next(-9, 10);
+
+//     if(randNums[i] > 0)
+//     {
+//         sumPos = sumPos + randNums[i];
+//     }
+//     else if(randNums[i] < 0)
+//     {
+//         sumNeg = sumNeg + randNums[i];
+//     }
+
+//     Console.WriteLine(randNums[i]);
+// }
+
+// Console.WriteLine(sumPos);
+// Console.WriteLine(sumNeg);
+
+
+// // Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] array1 = {-4, -8, 8, 2};
+// int[] arrayNew = new int[4];
+
+// for (int i = 0; i < array1.Length; i++)
+// {
+//     arrayNew[i] = array1[i] * -1;
+//     Console.WriteLine(arrayNew[i]);
+// }
+
+
+// // Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// // 4; массив [6, 7, 19, 345, 3] -> нет
+// // -3; массив [6, 7, 19, 345, 3] -> да
+
+// int[] ar = new int[5];
+// for (int i = 0; i < ar.Length; i++)
+// {
+//     Console.Write("Введите число в массив: ");
+//     ar[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.Write("Введите своё число на проверку: ");
+// int givenNumb = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+// for (int i = 0; i < ar.Length; i++)
+// {
+//     if(ar[i] == givenNumb)
+//     {
+//         Console.WriteLine("да");
+//         count++;
+//         break;
+//     }
+    
+// }
+
+// if(count == 0)
+//    Console.WriteLine("нет");
+
+
+
+// MUST SEE! :> D
+
+// // string s = "Hello World";
+// // string s1 = "Hello";
+// // string s2 = "World";
+
+// // int[] nums = {1, 2, 3, 4, 5};
+
+// // string s3 = string.Concat(s1, s2);
+// // string s3 = s1 + " " + s2;
+// // Console.WriteLine(s3);
+// // // конкатинация (сложение строк)
+
+// // string s5 = string.Join("-!-", nums); // объединение коллекции данных 
+// // (Из массива в строку)
+
+// // string s = "1, 2, 3, 4, 5, 6, 7";
+
+// // foreach (var item in s)
+// // {
+// //     Console.WriteLine(item);
+// // }
+
+// // Console.WriteLine(s5);
+
+// string s = "1, 2, 3, 4, 5, 6, 7";
+
+// // string[] numbs = s.Split(" ");
+// // Замена одного элемента на другой
+
+// string[] numbs = s.Split(new char[]{' ', '.', ','}, StringSplitOptions.RemoveEmptyEntries);
+// // (Разбивает строку на элементы)
+
+// int[] n = numbs.Select(Int32.Parse).ToArray();
+// // Создание массива из выходной последовательности
+
+// foreach (var item in numbs)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string s10 = "14, 21, 23, 54, 45, 66, 97";
+// int[] num1 = Array.ConvertAll(s10.Split(), int.Parse);
+
+// // 1) string s3 = string.Concat(s1, s2);
+// // (Конкатинация (сложение строк))
+
+// // 2) string s5 = string.Join("-!-", nums);
+// // (Вывод массива в строку)
+
+// // 3) string[] numbs = s.Split(new char[]{' ', '.', ','}, StringSplitOptions.RemoveEmptyEntries);
+// // ((Разбивает строку на элементы), а выбранные элементы - это разделитель)
+
+// // 4) int[] n = numbs.Select(Int32.Parse).ToArray());
+// // (Создание массива из выходной последовательности)
+
--- seminars/sem_6.4/Program.cs
+++ seminars/sem_6.4/Program.cs
@@ -1,139 +1,139 @@
-﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив
-// [1 2 3 4 5] -> [5 4 3 2 1]
-// [6 7 3 6] -> [6 3 7 6]
+﻿// // Задача 39: Напишите программу, которая перевернёт одномерный массив
+// // [1 2 3 4 5] -> [5 4 3 2 1]
+// // [6 7 3 6] -> [6 3 7 6]
 
-Console.Write("Enter how many elements of the array you want to create: ");
-int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter how many elements of the array you want to create: ");
+// int lengthOfArray = Convert.ToInt32(Console.ReadLine());
 
-int[] limits = {1, 10};
-int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
-{
-    int[] array = new int[arrayLength];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(limits[0], limits[1]);
-        Console.Write($"{array[i]} ");
-    }
-
-    Console.WriteLine();
-    return array;
-}
-
-int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
-
-int[] RevertArray(int[] array)
-{
-    for (int i = 0; i < array.Length / 2; i++)
-    {
-        int tmp = array[i];
-        array[i] = array[array.Length - 1 - i];
-        array[array.Length - 1 - i] = tmp;
-    }
-
-    return array;
-}
-
-int[] revAr = RevertArray(array);
-Console.WriteLine(String.Join(" ", array));
-
-// Задача 40: Программа проверки на существоание треугольника 
-
-Console.WriteLine("Examination of true triangle!");
-
-Console.Write("Write side a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Write side b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Write side c: ");
-int c = Convert.ToInt32(Console.ReadLine());
-
-
-if(a + b > c && a + c > b && b + c > a)
-{
-    Console.WriteLine("Exam for triangle was passed!!!)");
-}
-else
-{
-    Console.WriteLine("Exam for triangle was failed!!!(( ");
-}
-
-// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-
-// 45 -> 101101
-// 3  -> 11
-// 2  -> 10
-
-Console.Write("Write numb pls: ");
-int numb = Convert.ToInt32(Console.ReadLine());
-string numbs = string.Empty;
-
-while(numb > 0)
-{
-    numbs = numb % 2 + numbs;
-    numb = numb / 2;
-}
-
-Console.WriteLine(numbs);
-
-//
-// static void Task02()
+// int[] limits = {1, 10};
+// int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
 // {
-//     int number = PromptInput("Введите число: ");
-//     int[] array = new int[Count(number)];
-
-//     static int[] ReverseArray(int[] array)
+//     int[] array = new int[arrayLength];
+//     for (int i = 0; i < array.Length; i++)
 //     {
-
+//         array[i] = new Random().Next(limits[0], limits[1]);
+//         Console.Write($"{array[i]} ");
 //     }
 
-//     static void PrintArray(int[] array)
-//     {
-
-//     }
-
-//     int Count(int count)
-//     {
-    
-//     }
-
-//     int[] GetNumber(int[] array, int number)
-//     {
-
-//     }
-
+//     Console.WriteLine();
+//     return array;
 // }
 
-// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+// int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
 
-Console.Write("Enter how many elements of the array you want to create: ");
-int lengthOfArray = Convert.ToInt32(Console.ReadLine());
-int[] limits = {1, 10};
-int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
-{
-    int[] array = new int[arrayLength];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(limits[0], limits[1]);
-        Console.Write($"{array[i]} ");
-    }
+// int[] RevertArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         int tmp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = tmp;
+//     }
 
-    Console.WriteLine();
-    return array;
-}
+//     return array;
+// }
 
-int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
-int[] copy = new int[array.Length];
+// int[] revAr = RevertArray(array);
+// Console.WriteLine(String.Join(" ", array));
 
-void PrintArray(int[] array)
-{
-    Console.WriteLine($"{string.Join(" ", array)}");
-}
+// // Задача 40: Программа проверки на существоание треугольника 
 
-for (int i = 0; i < array.Length; i++)
-{
-    copy[i] = array[i];
-}
+// Console.WriteLine("Examination of true triangle!");
 
-PrintArray(copy);
+// Console.Write("Write side a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Write side b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Write side c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+
+// if(a + b > c && a + c > b && b + c > a)
+// {
+//     Console.WriteLine("Exam for triangle was passed!!!)");
+// }
+// else
+// {
+//     Console.WriteLine("Exam for triangle was failed!!!(( ");
+// }
+
+// // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// // 45 -> 101101
+// // 3  -> 11
+// // 2  -> 10
+
+// Console.Write("Write numb pls: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// string numbs = string.Empty;
+
+// while(numb > 0)
+// {
+//     numbs = numb % 2 + numbs;
+//     numb = numb / 2;
+// }
+
+// Console.WriteLine(numbs);
+
+// //
+// // static void Task02()
+// // {
+// //     int number = PromptInput("Введите число: ");
+// //     int[] array = new int[Count(number)];
+
+// //     static int[] ReverseArray(int[] array)
+// //     {
+
+// //     }
+
+// //     static void PrintArray(int[] array)
+// //     {
+
+// //     }
+
+// //     int Count(int count)
+// //     {
+    
+// //     }
+
+// //     int[] GetNumber(int[] array, int number)
+// //     {
+
+// //     }
+
+// // }
+
+// // Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+// Console.Write("Enter how many elements of the array you want to create: ");
+// int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+// int[] limits = {1, 10};
+// int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
+// {
+//     int[] array = new int[arrayLength];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(limits[0], limits[1]);
+//         Console.Write($"{array[i]} ");
+//     }
+
+//     Console.WriteLine();
+//     return array;
+// }
+
+// int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
+// int[] copy = new int[array.Length];
+
+// void PrintArray(int[] array)
+// {
+//     Console.WriteLine($"{string.Join(" ", array)}");
+// }
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     copy[i] = array[i];
+// }
+
+// PrintArray(copy);
