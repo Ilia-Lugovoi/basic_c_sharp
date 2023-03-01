fc7d6356cc92cf5756fb89333315372cbd8254fb
Ilya <ilya.lugovoy012@gmail.com>
2023-03-01 11:32:35 +0300

    Add task 4

--- seminars/sem_6.4/Program.cs
+++ seminars/sem_6.4/Program.cs
@@ -104,3 +104,36 @@ Console.WriteLine(numbs);
 //     }
 
 // }
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+Console.Write("Enter how many elements of the array you want to create: ");
+int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+int[] limits = {1, 10};
+int[] CreateArrayOfRandNumbs(int arrayLength, int[] limits)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(limits[0], limits[1]);
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+    return array;
+}
+
+int[] array = CreateArrayOfRandNumbs(lengthOfArray, limits);
+int[] copy = new int[array.Length];
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"{string.Join(" ", array)}");
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    copy[i] = array[i];
+}
+
+PrintArray(copy);
