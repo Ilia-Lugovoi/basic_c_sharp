0cbd98a8343a78c3f48b491c88197723a2e7ad23
Ilya <ilya.lugovoy012@gmail.com>
2023-03-02 09:50:35 +0300

    Add task 1 for hw 6.4

--- seminars/hw_6.4/task1/Program.cs
+++ seminars/hw_6.4/task1/Program.cs
@@ -4,3 +4,32 @@
 // 0, 7, 8, -2, -2 -> 2
 
 // 1, -7, 567, 89, 223-> 3
+
+int IntNumberRequest(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double DoubNumberRequest(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void Main(string[] args)
+{
+    int m = IntNumberRequest("Enter number of numbers: ");
+    int count = 0;
+    for (int i = 0; i <= m; i++)
+    {
+        double num = DoubNumberRequest($"Enter number {i}: ");
+    }
+
+    if(num > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Numbers greater than zero: {count}");
