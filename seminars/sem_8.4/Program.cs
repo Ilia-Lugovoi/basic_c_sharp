6e0cf83880342e8ef4a65a48c756989aae37adda
Ilya <ilya.lugovoy012@gmail.com>
2023-03-09 12:25:53 +0300

    Add task 1 for sem 8

---
+++ seminars/sem_8.4/Program.cs
@@ -0,0 +1,52 @@
+﻿// // Задача 53: Задайте двумерный массив.
+// // Напишите программу, которая поменяет местами первую и последнюю строку массива
+
+// Console.Write("Enter how many rows do you want: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter how many columns do you want: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[] limits = {-100, 101};
+
+// int[,] CreateTwoDimArrayOfRandNumbs(int rows, int columns, int[] limits)
+// {
+//     int[,] twoDimArray = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             twoDimArray[i, j] = new Random().Next(limits[0], limits[1]);
+//             Console.Write($"{twoDimArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+
+//     Console.WriteLine();
+//     return twoDimArray;
+// }
+
+// int[,] array = CreateTwoDimArrayOfRandNumbs(rows, columns, limits);
+
+// int[] temp = new int[columns];
+
+// for (int i = 0; i < columns; i++)
+// {
+//     temp[i] = array[0, i];
+//     array[0, i] = array[rows - 1, i];
+//     array[rows - 1, i] = temp[i];
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// PrintArray(array);
