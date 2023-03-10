f6c6df8212fd188ddb71a5611f7458879b62ace5
Ilya <ilya.lugovoy012@gmail.com>
2023-03-10 11:57:09 +0300

    Add class task 4

--- seminars/sem_8.4_pract/Program.cs
+++ seminars/sem_8.4_pract/Program.cs
@@ -10,9 +10,30 @@
         }
     }
 }
+Console.Write("Enter how many rows do you want: ");
+int rows = Convert.ToInt32(Console.ReadLine());
 
-int[,] arr = FillArray(4, 4);
-ShowDuoArray(arr);
+Console.Write("Enter how many columns do you want: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[] limits = {-10, 11};
+int[,] FillArray(int rows, int columns, int[] limits)
+{
+    int[,] twoDimArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            twoDimArray[i, j] = new Random().Next(limits[0], limits[1]);
+            Console.Write($"{twoDimArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    return twoDimArray;
+}
+int[,] arr = FillArray(rows, columns, limits);
 
 int[] FindMinEl(int[,] arr)
 {
@@ -59,7 +80,7 @@ void DeleteRowsColumns(int[,] arr, int[] index)
 int[] indexMinValue = FindMinEl(arr);
 DeleteRowsColumns(arr, indexMinValue);
 
-Console.ReadLine();
+Console.WriteLine();
 
 /*
 Задача 59: Задайте двумерный массив из целых чисел. 
