8bcf0e5e78c3464a26cbfe22eea95bb78bb603bb
Ilya <ilya.lugovoy012@gmail.com>
2023-03-16 08:49:34 +0300

    Add task 1 for hw 9

--- seminars/hw_9.4/task1/Program.cs
+++ seminars/hw_9.4/task1/Program.cs
@@ -4,3 +4,24 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = Prompt("Enter number pls: ");
+
+void DerivationNaturalNumbsFromN(int n)
+{
+    if(n != 0)
+    {
+        Console.Write($"{n} ");
+        DerivationNaturalNumbsFromN(n - 1);
+    }
+}
+
+DerivationNaturalNumbsFromN(n);
+
+Console.WriteLine();
