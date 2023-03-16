e060c6c911839c5a24d65b9c37653e852548fafa
Ilya <ilya.lugovoy012@gmail.com>
2023-03-16 09:20:07 +0300

    Add task 2 for hw 9

--- seminars/hw_9.4/task2/Program.cs
+++ seminars/hw_9.4/task2/Program.cs
@@ -3,6 +3,27 @@
 // в промежутке от M до N.
 
 // M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+// M = 4; N = 8. -> 30 (4, 5, 6, 7, 8)
 
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+int m = Prompt("Enter number M pls: ");
+int n = Prompt("Enter number N pls: ");
+int sum = 0;
+
+int FindSumFromMToN(int a, int b, int sum)
+{
+    if(a > b) return sum;
+    {
+        Console.Write($"{a} ");
+        sum += a;
+        return FindSumFromMToN(a + 1, b, sum);
+    }
+}
+
+sum = FindSumFromMToN(m, n, sum);
+Console.WriteLine($"Sum: {sum}");
